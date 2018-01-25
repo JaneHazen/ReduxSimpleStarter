865f44a1c1b50a372d969ed59cfdacf0ed92f2b8
Jane <iamjane.gmail.com>
2018-01-24 21:33:10 -0800

    hello world on the screen

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 //  Create a new component. This component should produce some HTML
 
-const App = function() {
-  return <div> HI! </div>;
+// fat arrow function is the same as function(){}... except that value of "this" inside function changes
+const App = () => {
+  return <div>HI!</div>;
 }
 
-// Put the component into the DOM
+// Put the component into the DOM (APP instance of the class defined above, second argument here is where we're putting it in the DOM from the HTML file)
+ReactDOM.render(<App/>, document.querySelector('.container') );
